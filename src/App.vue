4800d4873c6d92e60c53224d2f7ebe27c805bc41
Forjing <xiaodoubao1994@gmail.com>
2019-11-30 20:17:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <canvas id="canvas" ref="canvas"></canvas>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const that = this;
+    fetch("http://127.0.0.1:8080/mario.nes")
+      .then(r => r.arrayBuffer())
+      .then(data => {
+        this.drawNes(data);
+      });
+  },
+
+  methods: {
+    drawNes(bytes) {
+      /**
+       * @type {HTMLCanvasElement}
+       */
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext("2d");
+      console.log({ bytes });
+    }
+  }
+};
+</script>
+
+<style>
+#canvas {
+  border: 1px solid black;
+}
+</style>
